daf056445ab575c76b9e70212e27f9586d19af0c
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-17 05:10:55 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE dogs
+      
+    SQL
+  end
+  
+  
+end
