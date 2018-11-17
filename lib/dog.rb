71cb5b3239c314e188ff8609469f7aa15131e6f0
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-17 07:34:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,8 +64,8 @@ class Dog
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)
-    dog = Dog.new(id: result[0],name: result[1],breed: result[2])
-    return dog
+    Dog.new(id: result[0],name: result[1],breed: result[2])
+    
   end
   
 end
